c46473fd19618d7b7965458ed1e2f5aa0bd34e2c
Jordevá <jordevalucas@gmail.com>
2021-12-02 09:06:30 -0300

    generate examples

---
+++ src/examples/constants.ts
@@ -0,0 +1,95 @@
+export const EmployeesList = {
+  1: {
+    name: 'James',
+    role: 'sales adm',
+    created_at: '2012',
+  },
+  2: {
+    name: 'Mary',
+    role: 'logistics',
+    created_at: '2013',
+  },
+  3: {
+    name: 'Robert',
+    role: 'logistics',
+    created_at: '2014',
+  },
+  4: {
+    name: 'Patricia',
+    role: 'logistics',
+    created_at: '2015',
+  },
+  5: {
+    name: 'John',
+    role: 'sales adm',
+    created_at: '2016',
+  },
+  6: {
+    name: 'Jennifer',
+    role: 'sales adm',
+    created_at: '2017',
+  },
+  7: {
+    name: 'Michael',
+    role: 'logistics',
+    created_at: '2018',
+  },
+  8: {
+    name: 'Linda',
+    role: 'logistics',
+    created_at: '2019',
+  },
+  9: {
+    name: 'William',
+    role: 'logistics',
+    created_at: '2020',
+  },
+  10: {
+    name: 'Elizabeth',
+    role: 'logistics',
+    created_at: '2021',
+  },
+}
+
+export const DependentsList = {
+  1: {
+    name: 'David',
+    relationship: 'soon',
+  },
+  2: {
+    name: 'Barbara',
+    relationship: 'daughter',
+  },
+  3: {
+    name: 'Richard',
+    relationship: 'soon',
+  },
+  4: {
+    name: 'Susan',
+    relationship: 'daughter',
+  },
+  5: {
+    name: 'Joseph',
+    relationship: 'soon',
+  },
+  6: {
+    name: 'Jessica',
+    relationship: 'daughter',
+  },
+  7: {
+    name: 'Thomas',
+    relationship: 'soon',
+  },
+  8: {
+    name: 'Sarah',
+    relationship: 'daughter',
+  },
+  9: {
+    name: 'Charles',
+    relationship: 'soon',
+  },
+  10: {
+    name: 'Karen',
+    relationship: 'daughter',
+  },
+}
